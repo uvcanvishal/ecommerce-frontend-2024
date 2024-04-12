22d8c1c10212ddffb8873fdbadb7fa2b4b374d11
Vishal kumar singh <uvcanvishal@gmail.com>
2024-04-12 17:16:31 +0530

    Initial commit after frontend completion

---
+++ src/components/product-card.tsx
@@ -0,0 +1,41 @@
+import { FaPlus } from "react-icons/fa";
+import { server } from "../redux/store";
+import { CartItem } from "../types/types";
+
+type ProductProps = {
+  productId: string;
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  handler: (cartItem: CartItem) => string | undefined;
+};
+
+// const server="jbcjdn";
+
+const ProductCard = ({
+  productId,
+  price,
+  name,
+  photo,
+  stock,
+  handler,
+}: ProductProps) => {
+  return (
+    <div className="product-card">
+      <img src={`${server}/${photo}`} alt={name} />
+      <p>{name}</p>
+      <span>₹{price}</span>
+
+      <div>
+        <button
+          onClick={() => handler({ productId, price, name, photo, stock, quantity:1 })}
+        >
+          <FaPlus />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/pages/cart.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItemCard from "../components/cart-item";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { CartReducerInitialState } from "../types/reducer-types";
+import { CartItem } from "../types/types";
+import {
+  addToCart,
+  calculatePrice,
+  discountApplied,
+  removeCartItem,
+} from "../redux/reducer/cartReducer";
+import axios from "axios";
+import { server } from "../redux/store";
+
+const Cart = () => {
+  const dispatch = useDispatch();
+
+  const { cartItems, subtotal, tax, total, shippingCharges, discount } =
+    useSelector(
+      (state: { cartReducer: CartReducerInitialState }) => state.cartReducer
+    );
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
+
+  useEffect(() => {
+    const { token: cancelToken, cancel } = axios.CancelToken.source();
+
+    const timeOutId = setTimeout(() => {
+      axios
+        .get(`${server}/api/v1/payment/discount?coupon=${couponCode}`, {
+          cancelToken,
+        })
+        .then((res) => {
+          dispatch(discountApplied(res.data.discount));
+          setIsValidCouponCode(true);
+          dispatch(calculatePrice());
+        })
+        .catch((e) => {
+          dispatch(discountApplied(0));
+          setIsValidCouponCode(false);
+          dispatch(calculatePrice());
+        });
+    });
+
+    return () => {
+      clearTimeout(timeOutId);
+      cancel();
+      setIsValidCouponCode(false);
+    };
+  }, [couponCode]);
+
+  const incrementHandler = (cartItem: CartItem) => {
+    if (cartItem.quantity >= cartItem.stock) return;
+    dispatch(addToCart({ ...cartItem, quantity: cartItem.quantity + 1 }));
+  };
+  const decrementHandler = (cartItem: CartItem) => {
+    if (cartItem.quantity <= 1) return;
+    dispatch(addToCart({ ...cartItem, quantity: cartItem.quantity - 1 }));
+  };
+  const removeHandler = (productId: string) => {
+    dispatch(removeCartItem(productId));
+  };
+
+  useEffect(() => {
+    dispatch(calculatePrice());
+  }, [cartItems]);
+
+  return (
+    <div className="cart">
+      <main>
+        {cartItems.length > 0 ? (
+          cartItems.map((i, idx) => (
+            <CartItemCard
+              incrementHandler={incrementHandler}
+              decrementHandler={decrementHandler}
+              removeHandler={removeHandler}
+              key={idx}
+              cartItem={i}
+            />
+          ))
+        ) : (
+          <h1>No Items Added</h1>
+        )}
+      </main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em className="red"> - ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total: ₹{total}</b>
+        </p>
+
+        <input
+          type="text"
+          placeholder="Coupon Code"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ₹{discount} off using <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon <VscError />
+            </span>
+          ))}
+
+        {cartItems.length > 0 && <Link to="/shipping">Checkout</Link>}
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
